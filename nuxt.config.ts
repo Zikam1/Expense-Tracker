12d3aa909cba592bcc352b920fff952e29ebb133
Zikam1 <isaac.aku@cyphercrescent.com>
2024-07-24 10:42:27 +0100

    firebase-token

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss"]
+  modules: [
+    // ... other modules
+    '', "@nuxtjs/tailwindcss"
+  ],
 
+  vuefire: {
+    config: {
+      apiKey: "AIzaSyBfDH0tU6oxIlcbzZVg6IDRHmSlOzcUztY",
+      authDomain: "zikam-expense.firebaseapp.com",
+      projectId: "zikam-expense",
+      storageBucket: "zikam-expense.appspot.com",
+      messagingSenderId: "246667483441",
+      appId: "1:246667483441:web:fd37635f69be063b6df810",
+      measurementId: "G-5D7SHWZRNS",
+      // there could be other properties depending on the project
+    },
+  },
 })
--- pages/Signup.vue
+++ pages/Signup.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-screen flex items-center justify-center bg-gray-100">
     <div class="max-w-md w-full bg-white p-8 shadow-md rounded-lg">
-      <h2 class="text-2xl font-bold mb-6 text-gray-800">Login</h2>
+      <h2 class="text-2xl font-bold mb-6 text-gray-800">Sign Up</h2>
       <form @submit.prevent="handleSignup">
         <div class="mb-4">
           <label for="email" class="block text-gray-700 mb-2">Email</label>
@@ -13,16 +13,25 @@
             required
           />
         </div>
-       
+        <div class="mb-6">
+          <label for="password" class="block text-gray-700 mb-2">Password</label>
+          <input
+            type="password"
+            id="password"
+            v-model="password"
+            class="w-full px-3 py-2 border rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+            required
+          />
+        </div>
         <button
           type="submit"
           class="w-full py-2 px-4 bg-blue-500 text-white font-bold rounded-lg hover:bg-blue-600"
         >
-         Login
+          Sign Up
         </button>
         <p class="mt-4 text-gray-600">
           Already have an account?
-          <NuxtLink to="/login" class="text-blue-500 hover:underline">Signup</NuxtLink>
+          <NuxtLink to="/login" class="text-blue-500 hover:underline">Login</NuxtLink>
         </p>
       </form>
     </div>
